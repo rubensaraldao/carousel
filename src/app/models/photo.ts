78f2c89f5bdda672b8af32b45f61c1f43c421932
ruben sar <rubensaraldao@gmail.com>
2018-05-07 02:40:13 +0200

    Step 2 Add model

---
+++ src/app/models/photo.ts
@@ -0,0 +1,11 @@
+
+export class photo { 
+    public name: string ;
+    public url: string;
+
+    constructor(name: string, url: string){
+        this.name=name;
+        this.url=url;
+    }
+ }
+ 
